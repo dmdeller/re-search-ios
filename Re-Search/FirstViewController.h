3e4d3d21a80836529e11364337939fdc7446c95a
David Deller <david@horizon-nigh.org>
2014-07-19 13:06:48 -0400

    Create project

---
+++ Re-Search/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Re-Search
+//
+//  Created by David on 7/19/14.
+//  Copyright (c) 2014 David Deller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Re-Search/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Re-Search
+//
+//  Created by David on 7/19/14.
+//  Copyright (c) 2014 David Deller. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
