49d48402dfee9ef14c3ddb594de7f5ce3aad7061
David Deller <david@horizon-nigh.org>
2014-09-07 13:04:01 -0400

    Add embedded framework

---
+++ Re-SearchKit/Re-SearchKit.h
@@ -0,0 +1,19 @@
+//
+//  Re-SearchKit.h
+//  Re-SearchKit
+//
+//  Created by David on 9/7/14.
+//  Copyright (c) 2014 David Deller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Re-SearchKit.
+FOUNDATION_EXPORT double Re_SearchKitVersionNumber;
+
+//! Project version string for Re-SearchKit.
+FOUNDATION_EXPORT const unsigned char Re_SearchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Re_SearchKit/PublicHeader.h>
+
+
