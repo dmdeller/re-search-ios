a67389acf0b8f47b40bceeb7ceda3269c33745f0
David Deller <david@horizon-nigh.org>
2014-09-07 13:17:04 -0400

    Add embedded framework
    This type with no hypen in the name, since that seemed to break things

---
+++ ReSearchKit/ReSearchKit.h
@@ -0,0 +1,19 @@
+//
+//  ReSearchKit.h
+//  ReSearchKit
+//
+//  Created by David on 9/7/14.
+//  Copyright (c) 2014 David Deller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReSearchKit.
+FOUNDATION_EXPORT double ReSearchKitVersionNumber;
+
+//! Project version string for ReSearchKit.
+FOUNDATION_EXPORT const unsigned char ReSearchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReSearchKit/PublicHeader.h>
+
+
