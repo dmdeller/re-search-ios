65376676f3eb77344971ae4cb7b94321996c3b24
David Deller <david@horizon-nigh.org>
2014-09-10 20:07:45 -0400

    WIP - How To

--- ReSearch/WelcomeViewController.m
+++ ReSearch/WelcomeViewController.m
@@ -10,18 +10,41 @@
 
 @interface WelcomeViewController ()
 
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) IBOutlet UIView *howToView;
+
 @end
 
 @implementation WelcomeViewController
             
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [NSBundle.mainBundle loadNibNamed:@"HowTo" owner:self options:nil];
+    
+    [self.scrollView addSubview:self.howToView];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)updateViewConstraints
+{
+    [super updateViewConstraints];
+    
+    NSLayoutConstraint *widthConstraint1 = [NSLayoutConstraint constraintWithItem:self.howToView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
+    widthConstraint1.priority = UILayoutPriorityDefaultHigh;
+    
+    NSLayoutConstraint *widthConstraint2 = [NSLayoutConstraint constraintWithItem:self.howToView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationLessThanOrEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:450.0];
+    
+    [self.view addConstraint:widthConstraint1];
+    [self.howToView addConstraint:widthConstraint2];
+    
+    self.scrollView.contentSize = self.howToView.bounds.size;
 }
 
 @end
